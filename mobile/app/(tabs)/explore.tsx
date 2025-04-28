e88ee0a67f9b102266722c696bd6f7169bfd94a4
CoderWeiJ <1213586653@qq.com>
2025-04-28 09:20:25 +0800

    feat(mobile): 完成AI助手和首页展示收支记录的功能

--- mobile/app/(tabs)/explore.tsx
+++ mobile/app/(tabs)/explore.tsx
@@ -1,21 +1,52 @@
 
-import { useAuth } from "@/stores";
-import { useChat } from "@ai-sdk/react";
+import RecordCard from "@/components/RecordCard";
+import { useAuthStore } from "@/stores";
+import { Message, useChat } from "@ai-sdk/react";
 import { fetch as expoFetch } from "expo/fetch";
 import { View, TextInput, ScrollView, Text, SafeAreaView } from "react-native";
 
+function renderMessage(message: Message) {
+  if (message.role === 'user' || message.id === '1') {
+    return <Text>{message.content}</Text>
+  }
+  if (message.role === 'assistant') {
+    const parsedMessage = JSON.parse(message.content);
+    // AI 普通文本
+    if (parsedMessage.text) {
+      return <Text>{parsedMessage.text}</Text>
+    }
+    return <RecordCard record={parsedMessage.records} />
+  }
+}
+
 export default function App() {
-  const { session } = useAuth();
+  const session = useAuthStore(state => state.session);  
   const { messages, error, handleInputChange, input, handleSubmit } = useChat({
     fetch: expoFetch as unknown as typeof globalThis.fetch,
     api: `${process.env.EXPO_PUBLIC_API_URL}/chat`,
     onError: (error) => console.error(error, "ERROR"),
     streamProtocol: 'text',
-    body: { user_id: session?.user?.id },
-    initialMessages: [{ id: '-001', role: 'assistant', content: '你好，我是你的智账（智能记账）助手，有什么可以帮你的吗？'}]
+    body: { user_id: session?.user?.id},
+    initialMessages: [{ id: '1', role: 'assistant', content: '你好，我是你的智账（智能记账）助手，有什么可以帮你的吗？'}],
+    headers: { 
+      'Authorization': `Bearer ${session?.access_token}`
+    },
+    onFinish: (message, _options) => {
+      try {
+        const parseMessage = JSON.parse(message.content);
+        if (!parseMessage.text) {
+          // fet
+        }
+      } catch (error) {
+        
+      }
+    }
   });
 
-  if (error) return <Text>{error.message}</Text>;
+
+  if (error){
+    return <Text className="mt-10">{error.message}</Text>;
+  }
 
   return (
     <SafeAreaView style={{ height: "100%", marginTop: 30 }}>
@@ -32,7 +63,7 @@ export default function App() {
             <View key={m.id} style={{ marginVertical: 8 }}>
               <View>
                 <Text style={{ fontWeight: 700, alignSelf: m.role === 'assistant' ? 'flex-start': 'flex-end' }} className='rounded-xl py-2 px-3 mb-1'>{m.role === 'assistant' ? '智账助手': '我'}</Text>
-                <Text className={`p-4 rounded-xl ${m.role === 'assistant' ? 'bg-blue-300': 'bg-slate-200'}`}>{m.content}</Text>
+                <View className={`p-4 rounded-xl ${m.role === 'assistant' ? '': 'bg-slate-200'}`}>{renderMessage(m)}</View>                
               </View>
             </View>
           ))}
--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,13 +1,72 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { View, Text, Pressable, ScrollView } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import RecordCard from "@/components/RecordCard";
-
+import axios from "axios";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { create } from "zustand";
+import { Session } from "@supabase/supabase-js";
+import { useAuthStore } from "@/stores";
+
+interface RecordStore {
+  records: Array<{
+    id: number;
+    createAt: Date;
+    title: string;
+    amount: number;
+    userId: string;
+    date: Date;
+  }>;
+  fetchRecords: (date: Date, session: Session | null) => void;
+}
+
+const useRecord = create<RecordStore>((set) => ({
+  records: [],
+  fetchRecords: (date, session) => {
+    if (!session?.user?.id) {
+      return;
+    }
+    console.log(`${process.env.EXPO_PUBLIC_API_URL}/record`);
+    const headers = { Authorization: `Bearer ${session?.access_token}` };
+    axios
+      .post(
+        `${process.env.EXPO_PUBLIC_API_URL}/record`,
+        { user_id: session.user.id, date: date.toISOString().split("T")[0] },
+        { headers }
+      )
+      .then((res) => {
+        set({ records: res.data });
+      })
+      .catch((error) => {
+        console.log("报错了", error);
+      });
+  },
+}));
 
 export default function HomeScreen() {
+  const session = useAuthStore((state) => state.session);
   const [date, setDate] = useState(new Date());
+  const records = useRecord((state) => state.records);
+  const fetchRecords = useRecord((state) => state.fetchRecords);
   const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const { income, outcome } = useMemo(() => {
+    let income = 0;
+    let outcome = 0;
+    records.forEach((record) => {
+      const amount = record.amount / 100;
+      if (amount > 0) {
+        income += amount;
+      } else {
+        outcome += amount;
+      }
+    });
+    return { income, outcome: Math.abs(outcome) };
+  }, [records]);
+
+  useEffect(() => {
+    fetchRecords(date, session);
+  }, [date, session, fetchRecords]);
   return (
     <SafeAreaView className="flex-1 flex gap-4 mx-4 mt-2">
       {/* date */}
@@ -37,13 +96,13 @@ export default function HomeScreen() {
         <View className="flex-1 bg-green-50 rounded-lg p-4 flex items-center justify-center">
           <View className="w-full flex flex-row justify-between">
             <Text className="font-bold">收入</Text>
-            <Text className="text-green-500">1005</Text>
+            <Text className="text-green-500">{income}</Text>
           </View>
         </View>
         <View className="flex-1 bg-green-50 rounded-lg p-4 flex items-center justify-center">
           <View className="w-full flex flex-row justify-between">
             <Text className="font-bold">支出</Text>
-            <Text className="text-red-500">500</Text>
+            <Text className="text-red-500">{outcome}</Text>
           </View>
         </View>
       </View>
@@ -51,24 +110,25 @@ export default function HomeScreen() {
       {/* 详细记录 */}
       <View className="flex-1 bg-gray-100 rounded-lg py-4 px-1 gap-2">
         <Text className="text-gray-500">详细记录</Text>
-        <ScrollView className="flex-1">
-          <RecordCard
-            record={{
-              id: 1,
-              title: "收入",
-              amount: 1000,
-              createdAt: "2025-04-22",
-            }}
-          ></RecordCard>
-          <RecordCard
-            record={{
-              id: 1,
-              title: "支出",
-              amount: -1000,
-              createdAt: "2025-04-20",
-            }}
-          ></RecordCard>
-        </ScrollView>
+        {records.length > 0 && (
+          <ScrollView className="flex-1">
+            {records.map((record) => {
+              return (
+                <RecordCard
+                  key={record.id}
+                  record={{
+                    id: record.id,
+                    title: record.title,
+                    amount: record.amount / 100,
+                    createdAt: record.createAt.toString(),
+                  }}
+                />
+              );
+            })}
+          </ScrollView>
+        )}
+        {records.length === 0 && <View className="flex-1 flex justify-center items-center">
+          <Text className="color-[#111] opacity-50">暂无记录</Text></View>}
       </View>
     </SafeAreaView>
   );
